7efc384699ffc5d3322622831ff264b052834d33
caroline <gensac.caroline@gmail.com>
2024-08-27 16:42:27 +0200

    tilte + input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+    <div className="container">
+      <h2>To Do List</h2>
+      <div className="top">
+          <input
+            type="text"
+            placeholder="What do you need to do ? "
+            // value={text}
+            // onChange={(e) => setText(e.target.value)}
+          />
+          </div>
+      </div>
+      </div>
   );
 }
 
