f78131a9b1d96908de55960cc1cd217cb755fc67
caroline <gensac.caroline@gmail.com>
2024-08-27 14:43:46 +0200

    branchement mongo

--- backend/Server.js
+++ backend/Server.js
@@ -6,4 +6,11 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.port || 8080
 
+
+mongoose 
+.connect(process.env.MONGODB_URL)
+.then(()=> console.log(`Connected To mONGODB...`))
+.catch((err) => console.log(err))
+
+
 app.listen(PORT, ()=> console.log(`Listening on ${PORT}`))
